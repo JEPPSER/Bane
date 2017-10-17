758dad81d12611b992e5aef4cdb176978df54cd3
Jesper Bergström <jesper.b96@gmail.com>
2017-10-17 03:47:17 +0200

    Added player rotation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,44 +6,51 @@ public class PlayerScript : MonoBehaviour {
     private bool[] directions = { false, false, false, false };
     private float x = 0;
     private float y = 0;
+    private float sensitivity = 5f;
+    private float angle = 0f;
 
     // Use this for initialization
     void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if(!isLocalPlayer)
-       // {
-       //     return;
-        //}
 
         for(int i=0; i < keys.Length; i++)
         {
             directions[i] = Input.GetKey(keys[i]);
         }
-        move(directions); 
+
+        move(directions);
+        rotate();
 	}
 
     private void move(bool[] directions)
     {
         if (directions[0])
         {
-            y -= 2;
+            y += 1;
         }
         if (directions[1])
         {
-            x -= 2;
+            x -= 1;
         }
         if (directions[2])
         {
-            y += 2;
+            y -= 1;
         }
         if (directions[3])
         {
-            x += 2;
+            x += 1;
         }
-        transform.SetPositionAndRotation(new Vector3(x, y), new Quaternion(0, 0, 0, 0));
+        transform.position = new Vector3(x, y);
+    }
+
+    private void rotate()
+    {
+        float xRot = Input.GetAxis("Mouse X");
+        angle += xRot;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
     }
 }
